b94dccc26da314ba2e3dc80dea9e5d6b81491c9a
DenisFedorov <prince.fedorov@yandex.ru>
2023-03-08 03:42:58 +0400

    refactor: создать проект

---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.*;
+
+@RestController
+@Slf4j
+public class UserController {
+
+    private final Map<Long, User> userStorage = new HashMap();
+    private Long counter = 1L;
+
+    @PostMapping("/users")
+    public User addUser(@Valid @RequestBody User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        user.setId(counter++);
+        userStorage.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@Valid @RequestBody User user) {
+        //userStorage.removeIf(e -> e.getId().equals(user.getId()));
+        if (userStorage.containsKey(user.getId())) {
+            userStorage.put(user.getId(), user);
+            return user;
+        } else {
+            throw new  ValidationException("Не тот код");
+        }
+    }
+
+    @GetMapping("/users")
+    public Set<User> findAll() {
+        log.debug("Текущее количество пользователей: {}", userStorage.size());
+        return new HashSet<>(userStorage.values());
+    }
+}
---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+
+    @Positive
+    private Long id;
+    @NotBlank
+    private String name;
+    @Size(max = 200)
+    private String description;
+    private LocalDate releaseDate;
+    @Positive
+    private Long duration;
+
+}
